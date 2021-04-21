5f0bb341d16c52d6c66523be5102b36d33d2c1c8
majkowsw <majkowsw@uni.coventry.ac.uk>
2021-04-21 13:30:33 +0100

    added database to registerFragment

--- app/src/main/java/com/example/navigation/registerFragment.java
+++ app/src/main/java/com/example/navigation/registerFragment.java
@@ -4,21 +4,76 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
-public class registerFragment extends Fragment {
+import com.example.navigation.ui.database.Message;
+import com.example.navigation.ui.database.myDbAdapter;
 
-    public registerFragment() {
-        // Required empty public constructor
-    }
+public class registerFragment extends Fragment implements View.OnClickListener {
+
+    EditText Name, Pass , Email, Re_Pass;
+    myDbAdapter helper;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
-
+        View layout = inflater.inflate(R.layout.fragment_register, container, false);
+        Button button = (Button) layout.findViewById(R.id.btn_register);
+        button.setOnClickListener(this);
+        return layout;
     }
+
+    @Override
+    public void onClick(View view) {
+        register();
+    }
+
+    public void register()
+    {
+        Name = (EditText) getActivity().findViewById(R.id.et_name);
+        Pass = (EditText) getActivity().findViewById(R.id.et_password);
+        Re_Pass = (EditText) getActivity().findViewById(R.id.et_repassword);
+        Email = (EditText) getActivity().findViewById(R.id.et_email);
+
+        helper = new myDbAdapter(getContext());
+        Button button = getActivity().findViewById(R.id.btn_register);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+        String t1 = Name.getText().toString();
+        String t2 = Pass.getText().toString();
+        String t3 = Re_Pass.getText().toString();
+        String t4 = Email.getText().toString();
+
+        if(t1.isEmpty() || t2.isEmpty() || t3.isEmpty() || t4.isEmpty()){
+            Message.message(getActivity(), "Fill the form");
+        }
+        else
+        {
+            long id = helper.insertData(t1,t2,t3,t4);
+            if(id<=0)
+            {
+                Message.message(getActivity(),"Insertion Unsuccessful");
+                Name.setText("");
+                Pass.setText("");
+                Re_Pass.setText("");
+                Email.setText("");
+            } else
+            {
+                Message.message(getActivity(),"Insertion Successful");
+                Name.setText("");
+                Pass.setText("");
+                Re_Pass.setText("");
+                Email.setText("");
+            }
+        }
+    }});
+    }
+
 
 }
---
+++ app/src/main/java/com/example/navigation/ui/database/myDbAdapter.java
@@ -0,0 +1,104 @@
+package com.example.navigation.ui.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class myDbAdapter {
+    myDbHelper myhelper;
+    public myDbAdapter(Context context)
+    {
+        myhelper = new myDbHelper(context);
+    }
+
+    public long insertData(String name, String pass, String re_pass, String email)
+    {
+        SQLiteDatabase dbb = myhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(myDbHelper.NAME, name);
+        contentValues.put(myDbHelper.MyPASSWORD, pass);
+        contentValues.put(myDbHelper.RE_PASSWORD, re_pass);
+        contentValues.put(myDbHelper.EMAIL, email);
+        long id = dbb.insert(myDbHelper.TABLE_NAME, null , contentValues);
+        return id;
+    }
+
+    public String getData()
+    {
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        String[] columns = {myDbHelper.UID, myDbHelper.NAME, myDbHelper.MyPASSWORD};
+        Cursor cursor =db.query(myDbHelper.TABLE_NAME,columns,null,null,null,null,null);
+        StringBuffer buffer= new StringBuffer();
+        while (cursor.moveToNext())
+        {
+            int cid =cursor.getInt(cursor.getColumnIndex(myDbHelper.UID));
+            String name =cursor.getString(cursor.getColumnIndex(myDbHelper.NAME));
+            String password =cursor.getString(cursor.getColumnIndex(myDbHelper.MyPASSWORD));
+            buffer.append(cid+ "   " + name + "   " + password +" \n");
+        }
+        return buffer.toString();
+    }
+
+    public  int delete(String uname)
+    {
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        String[] whereArgs ={uname};
+
+        int count =db.delete(myDbHelper.TABLE_NAME , myDbHelper.NAME+" = ?",whereArgs);
+        return  count;
+    }
+
+    public int updateName(String oldName , String newName)
+    {
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(myDbHelper.NAME,newName);
+        String[] whereArgs= {oldName};
+        int count =db.update(myDbHelper.TABLE_NAME,contentValues, myDbHelper.NAME+" = ?",whereArgs );
+        return count;
+    }
+
+    static class myDbHelper extends SQLiteOpenHelper
+    {
+        private static final String DATABASE_NAME = "mylocalDatabase";    // Database Name
+        private static final String TABLE_NAME = "myTable";   // Table Name
+        private static final int DATABASE_Version = 1;// Database Version
+        private static final String UID="_id";     // Column I (Primary Key)
+        private static final String NAME = "Name";    //Column II
+        private static final String MyPASSWORD= "Password";    // Column III
+        private static final String RE_PASSWORD= "Re_password"; // Column IV
+        private static final String EMAIL= "Email"; // Column V
+        private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+
+                " ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255) ,"+ MyPASSWORD+" VARCHAR(225),"+RE_PASSWORD+" VARCHAR(225),"+ EMAIL+" VARCHAR(225));";
+        private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;
+        private Context context;
+
+        public myDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_Version);
+            this.context=context;
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+
+            try {
+                db.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+                Message.message(context,""+e);
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            try {
+                Message.message(context,"OnUpgrade");
+                db.execSQL(DROP_TABLE);
+                onCreate(db);
+            }catch (Exception e) {
+                Message.message(context,""+e);
+            }
+        }
+    }
+}
