f0838a28499294653266c52bc1f686e33c1db5cd
majkowsw <majkowsw@uni.coventry.ac.uk>
2021-04-21 12:10:55 +0100

    Removed Redundant files

--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -23,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home,
                 R.id.navigation_maps,
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications
+                R.id.navigation_dashboard
         ).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
