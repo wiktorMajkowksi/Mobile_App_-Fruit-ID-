8f0933cd30f2f961871c70ea4e5092a0cda5637b
majkowsw <majkowsw@uni.coventry.ac.uk>
2021-04-22 14:49:35 +0100

    improved screen orientation

--- app/src/main/java/com/example/navigation/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navigation/ui/home/HomeFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,8 +19,7 @@ public class HomeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         TextView textView = root.findViewById(R.id.welcome_text);
-        textView.setText("This is my app");
-        ImageView imageView = root.findViewById(R.id.app_logo);
+        textView.setText("Welcome to FruitID");
 
         return root;
     }
