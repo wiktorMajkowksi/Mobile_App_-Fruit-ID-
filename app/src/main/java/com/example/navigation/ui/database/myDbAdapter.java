6eca0c84ab851f82eabb5c75d553545d940bc0b3
majkowsw <majkowsw@uni.coventry.ac.uk>
2021-04-22 11:53:18 +0100

    added comments

--- app/src/main/java/com/example/navigation/ui/database/myDbAdapter.java
+++ app/src/main/java/com/example/navigation/ui/database/myDbAdapter.java
@@ -16,6 +16,11 @@ public class myDbAdapter {
         myhelper = new myDbHelper(context);
     }
 
+    /*
+           Inserts data into database.
+           Takes 4 string arguments, those strings are then
+           injected in the database in accordance to specified fields
+     */
     public long insertData(String name, String pass, String re_pass, String email)
     {
         SQLiteDatabase dbb = myhelper.getWritableDatabase();
@@ -27,25 +32,15 @@ public class myDbAdapter {
         long id = dbb.insert(myDbHelper.TABLE_NAME, null , contentValues);
         return id;
     }
-
-    public String getData()
-    {
-        SQLiteDatabase db = myhelper.getWritableDatabase();
-        String[] columns = {myDbHelper.UID, myDbHelper.NAME, myDbHelper.MyPASSWORD};
-        Cursor cursor =db.query(myDbHelper.TABLE_NAME,columns,null,null,null,null,null);
-        StringBuffer buffer= new StringBuffer();
-        while (cursor.moveToNext())
-        {
-            int cid =cursor.getInt(cursor.getColumnIndex(myDbHelper.UID));
-            String name =cursor.getString(cursor.getColumnIndex(myDbHelper.NAME));
-            String password =cursor.getString(cursor.getColumnIndex(myDbHelper.MyPASSWORD));
-            buffer.append(cid+ "   " + name + "   " + password +" \n");
-        }
-        return buffer.toString();
-    }
+    /*
+        Method for authorising login attempt.
+        Extracts Email and Password of each entry and joins them into
+        one string separated with " ",
+        the string is then added to arraylist, it is done for whole database
+     */
     public ArrayList getAuth(){
         SQLiteDatabase db = myhelper.getWritableDatabase();
-        String[] columns = {myDbHelper.UID, myDbHelper.EMAIL, myDbHelper.MyPASSWORD};
+        String[] columns = {myDbHelper.EMAIL, myDbHelper.MyPASSWORD};
         Cursor cursor =db.query(myDbHelper.TABLE_NAME,columns,null,null,null,null,null);
         ArrayList buffer= new ArrayList();
         while (cursor.moveToNext())
@@ -57,45 +52,33 @@ public class myDbAdapter {
         return buffer;
     }
 
-    public  int delete(String uname)
-    {
-        SQLiteDatabase db = myhelper.getWritableDatabase();
-        String[] whereArgs ={uname};
-
-        int count =db.delete(myDbHelper.TABLE_NAME , myDbHelper.NAME+" = ?",whereArgs);
-        return  count;
-    }
-
-    public int updateName(String oldName , String newName)
-    {
-        SQLiteDatabase db = myhelper.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(myDbHelper.NAME,newName);
-        String[] whereArgs= {oldName};
-        int count =db.update(myDbHelper.TABLE_NAME,contentValues, myDbHelper.NAME+" = ?",whereArgs );
-        return count;
-    }
-
     static class myDbHelper extends SQLiteOpenHelper
     {
-        private static final String DATABASE_NAME = "mylocalDatabase";    // Database Name
-        private static final String TABLE_NAME = "myTable";   // Table Name
-        private static final int DATABASE_Version = 1;// Database Version
-        private static final String UID="_id";     // Column I (Primary Key)
-        private static final String NAME = "Name";    //Column II
-        private static final String MyPASSWORD= "Password";    // Column III
-        private static final String RE_PASSWORD= "Re_password"; // Column IV
-        private static final String EMAIL= "Email"; // Column V
+        private static final String DATABASE_NAME = "mylocalDatabase";                              // Database Name
+        private static final String TABLE_NAME = "myTable";                                         // Table Name
+        private static final int DATABASE_Version = 1;                                              // Database Version
+        private static final String UID="_id";                                                      // Column I (Primary Key)
+        private static final String NAME = "Name";                                                  // Column II
+        private static final String MyPASSWORD= "Password";                                         // Column III
+        private static final String RE_PASSWORD= "Re_password";                                     // Column IV
+        private static final String EMAIL= "Email";                                                 // Column V
+        //Creates table
         private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+
                 " ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255) ,"+ MyPASSWORD+" VARCHAR(225),"+RE_PASSWORD+" VARCHAR(225),"+ EMAIL+" VARCHAR(225));";
-        private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;
+        private static final String DROP_TABLE ="DROP TABLE IF EXISTS "+TABLE_NAME;                 // Drops tables
         private Context context;
 
+        /*
+            extracts context of a given database
+         */
         public myDbHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_Version);
             this.context=context;
         }
 
+        /*
+          creates SQLiteDatabase
+        */
         public void onCreate(SQLiteDatabase db) {
 
             try {
