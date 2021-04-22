680dec77ba5ec6b173c611c9d42edcae277d6f5f
majkowsw <majkowsw@uni.coventry.ac.uk>
2021-04-22 18:05:38 +0100

    added customised icon

--- app/src/main/java/com/example/navigation/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navigation/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.navigation.ui.home;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,6 +22,8 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         TextView textView = root.findViewById(R.id.welcome_text);
         textView.setText("Welcome to FruitID");
+        ImageView imageView = root.findViewById(R.id.app_logo);
+        imageView.setImageBitmap(BitmapFactory.decodeFile("D:\\Year3\\AppDEV\\V1\\V1Complete\\app\\src\\main\\res\\mipmap-anydpi-v26"));
 
         return root;
     }
