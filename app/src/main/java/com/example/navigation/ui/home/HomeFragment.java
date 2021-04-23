4332b6cab417ae5af2c6bef917441e076996d1cd
majkowsw <majkowsw@uni.coventry.ac.uk>
2021-04-23 09:43:01 +0100

    optimised imports

--- app/src/main/java/com/example/navigation/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navigation/ui/home/HomeFragment.java
@@ -1,11 +1,9 @@
 package com.example.navigation.ui.home;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
